9286cb5811036c0e3ea8918feb769753589d604a
CJ Celimen <cjdcelimen@gmail.com>
2020-10-13 18:23:36 +0800

    Updates

---
+++ precache-manifest.fc840b1626e0617dc0c2cb3a4315d5ed.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dde509d2a20b6dd49faa7bedccc56eef",
+    "url": "/mytech/index.html"
+  },
+  {
+    "revision": "b07ae01bb324257e2716",
+    "url": "/mytech/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "efb443373b84819aae00",
+    "url": "/mytech/static/js/2.d6cb8cdb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/mytech/static/js/2.d6cb8cdb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b07ae01bb324257e2716",
+    "url": "/mytech/static/js/main.fa0f45a5.chunk.js"
+  },
+  {
+    "revision": "91ef68f0572681fdd3de",
+    "url": "/mytech/static/js/runtime-main.ae9ec37e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/mytech/static/media/logo.5d5d9eef.svg"
+  }
+]);
